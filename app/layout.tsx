c82c95c1509a61fc0100dd4d48aeb88491e62de0
seolleung2 <seolleung22@gmail.com>
2023-07-17 23:48:57 +0900

    add: Register 모달 내 body 에 Heading, Input 컴포넌트 생성 및 적용. react-hot-toast 적용 - Heading, Input 컴포넌트 생성 및 RegisterModal 내 body 에 적용 - react-hot-toast 설치 및 ToasterProvider 컴포넌트 생성 후 메인 layout 에 적용. - api 호출 후 try-catch 문의 catch 에서 toast.error 를 통해 에러 alert 를 상단에 보여지도록 적용.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from 'next/font/google';
 import Navbar from '@components/navbar';
 import ClientOnly from '@components/ClientOnly';
 import RegisterModal from '@components/modals/RegisterModal';
+import ToasterProvider from '@providers/ToasterProvider';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
